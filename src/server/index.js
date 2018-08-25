93fdae8ef65b7c5dca8f80606e4d801914610fd9
Bognar Junior <bognar_junior@yahoo.com.br>
2018-08-25 15:45:46 -0300

    Implementando servidor
    Importando os pacotes que serão utilizados no servidor, implementando servidor inicial para teste

--- src/server/index.js
+++ src/server/index.js
@@ -1,10 +1,10 @@
 const app = require('http').createServer();
-const io = Module.exports.io = require('socket.io')(app);
+const io = module.exports.io = require('socket.io')(app);
 
 const SocketManager = require('./SocketManager');
 
 const PORT = process.env.port || 3231;
 
-io.connection('connection', SocketManager);
+io.on('connection', SocketManager);
 
 app.listen(PORT, () => console.log(`Servidor conectado a porta ${PORT}`));
