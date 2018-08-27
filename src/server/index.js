3b33b0abb71e258b54ff4755043eb219e7f36026
Bognar Junior <bognar_junior@yahoo.com.br>
2018-08-27 11:04:10 -0300

    Adicionando express
    Adicionando express no projeto

--- src/server/index.js
+++ src/server/index.js
@@ -1,10 +1,14 @@
-const app = require('http').createServer();
-const io = module.exports.io = require('socket.io')(app);
+const express = require('express');
+const app = express();
+const server = require('http').Server(app);
+
+const io = module.exports.io = require('socket.io')(server);
 
 const SocketManager = require('./SocketManager');
 
 const PORT = process.env.port || 3231;
 
+app.use(express.static(__dirname + './../../build'));
 io.on('connection', SocketManager);
 
-app.listen(PORT, () => console.log(`Servidor conectado a porta ${PORT}`));
+server.listen(PORT, () => console.log(`Servidor conectado a porta ${PORT}`));
