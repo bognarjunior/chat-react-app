b11175584ae46423531e12f5aace7d55b62c5b9a
Bognar Junior <bognar_junior@yahoo.com.br>
2018-08-27 10:32:41 -0300

    Correção
    Corrigindo erro quando usuário saia do chat

--- src/components/Layout/Layout.js
+++ src/components/Layout/Layout.js
@@ -4,7 +4,7 @@ import io from 'socket.io-client';
 import LoginForm from './../Login/LoginForm';
 import ChatContainer from './../Chat/ChatContainer';
 
-import { USER_CONNECTED, LOGOUT } from './../../Events';
+import { USER_CONNECTED, LOGOUT, VERIFY_USER } from './../../Events';
 
 const socketURL = "http://192.168.15.13:3231/";
 
@@ -27,6 +27,9 @@ export default class Layout extends Component {
     const socket = io(socketURL);
 
     socket.on('connect', () => {
+      if (this.state.user) {
+        this.reconnect(socket);
+      }
     });
     this.setState({
       socket
@@ -47,6 +50,19 @@ export default class Layout extends Component {
     this.setState({
       user: null
     })
+  }
+
+  reconnect = (socket) => {
+    const { user } = this.state;
+    socket.emit(VERIFY_USER, user.name, ({ isUser, name }) => {
+      if (isUser) {
+        this.setState({
+          user: null
+        })
+      } else {
+        this.setUser(user);
+      }
+    });
   }
 
   render() {
